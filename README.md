ab1b73e1cbbf7c9ed6befa1e0c500908069558ac
Gyeongrok-Jang <jjkr4966@gmail.com>
2024-11-17 20:47:49 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Robot platform : N1
+
+LIDAR : YDLIDAR x4 or YDLIDAR x4 Pro
+
+ROS ver : ROS2 Humble
+
+Where to use : Computer
---
+++ stella_navigation2/launch/navigation2.launch.py
@@ -0,0 +1,88 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # 기본 설정값들
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    map_dir = LaunchConfiguration(
+        'map',
+        default=os.path.join(
+            get_package_share_directory('stella_navigation2'),
+            'map',
+            '/home/jang/colcon_ws/src/STELLA_N1_REMOTEPC_X4_ROS2_v2.0-Humble/stella_navigation2/map/my_map.yaml'))
+
+    param_dir = LaunchConfiguration(
+        'params_file',
+        default=os.path.join(
+            get_package_share_directory('stella_navigation2'),
+            'param',
+            'stella.yaml'))
+
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+
+    rviz_config_dir = os.path.join(
+        get_package_share_directory('stella_navigation2'),
+        'rviz',
+        'stella_navigation2.rviz')
+
+    ekf_param_file = os.path.join(
+        get_package_share_directory("stella_navigation2"),
+        'param',
+        'ekf.yaml')
+
+    # Launch Description 반환
+    return LaunchDescription([
+
+        # 맵 파일 경로 설정
+        DeclareLaunchArgument(
+            'map',
+            default_value=map_dir,
+            description='Full path to map file to load'),
+
+        # 파라미터 파일 경로 설정
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=param_dir,
+            description='Full path to param file to load'),
+
+        # 시뮬레이션 시간 사용 여부 설정
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+
+        # Nav2 bringup 실행
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
+            launch_arguments={
+                'map': map_dir,
+                'use_sim_time': use_sim_time,
+                'params_file': param_dir}.items(),
+        ),
+
+        # EKF 필터 노드 실행
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[ekf_param_file],
+        ),
+
+        # RViz2 실행
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen'),
+    ])
+
